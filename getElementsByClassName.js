113062e80462fd6119400be7f1e45fdf82137c95
savybrandt <savybrandt@gmail.com>
2016-12-09 12:56:09 -0800

    completed in wrong file, cut and pasted

--- getElementsByClassName.js
+++ getElementsByClassName.js
@@ -4,7 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var results = [];
+  var doc = document.body;
+
+  function getClass(doc, results, className){
+    var children = doc.childNodes
+    for (var i = 0; i < children.length; i++){
+        var classes = children[i].classList;
+        if (classes && classes.contains(className)) {
+            results.push(children[i]);
+        }
+        if (children[i].childNodes[0]){
+            getClass(children[i], results, className);
+        }
+    }
+  }
+  getClass(document, results, className);
+  return results;
 };
